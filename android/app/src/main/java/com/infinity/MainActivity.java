85c403939330f0b79bae2a15e33e2e4980918d3e
Jhoy1992 <jhonatanascari@gmail.com>
2020-12-10 10:50:13 -0300

    feat: Add build configs and splash

--- android/app/src/main/java/com/infinity/MainActivity.java
+++ android/app/src/main/java/com/infinity/MainActivity.java
@@ -1,28 +1,30 @@
 package com.infinity;
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 import io.wazo.callkeep.RNCallKeepModule; 
 import androidx.annotation.NonNull;
+import android.os.Bundle;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends ReactActivity {  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
+  }
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
   @Override
   protected String getMainComponentName() {
     return "infinity";
   }  
-
-  // Permission results
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case RNCallKeepModule.REQUEST_READ_PHONE_STATE:
-                RNCallKeepModule.onRequestPermissionsResult(requestCode, permissions, grantResults);
-                break;
-        }
+  
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    switch (requestCode) {
+      case RNCallKeepModule.REQUEST_READ_PHONE_STATE:
+        RNCallKeepModule.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        break;
     }
+   }    
 }
